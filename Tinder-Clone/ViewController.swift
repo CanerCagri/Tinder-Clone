81fa59149c968b2c46dcfa434e63e56229102c31
CanerCagri <canerrcagrii@gmail.com>
2023-04-14 15:34:46 +0300

    UI created with Stack Views.

--- Tinder-Clone/ViewController.swift
+++ Tinder-Clone/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        let subViews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { color in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subViews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+        stackView.axis = .vertical
+//        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        stackView.frame = view.bounds
+        
+        stackView.fillSuperview()
     }
-
-
 }
-
