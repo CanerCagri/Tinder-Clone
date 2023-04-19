ba3994fa0b54555c2496e78aedf276f9023f4ee4
CanerCagri <canerrcagrii@gmail.com>
2023-04-19 14:05:39 +0300

    Card dismiss and rotational movements in CardView.

--- Tinder-Clone/Views/CardView.swift
+++ Tinder-Clone/Views/CardView.swift
@@ -10,12 +10,12 @@ import UIKit
 class CardView: UIView {
     
     private let imageView = UIImageView(image: UIImage(named: "lady5c"))
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         layer.cornerRadius = 10
         clipsToBounds = true
- 
+        
         addSubview(imageView)
         imageView.fillSuperview()
         
@@ -30,14 +30,45 @@ class CardView: UIView {
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
         switch gesture.state {
         case .changed:
-            let transform = gesture.translation(in: nil)
-            self.transform = CGAffineTransform(translationX: transform.x, y: transform.y)
+            handleChanged(gesture)
         case .ended:
-            UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut) {
-                self.transform = .identity
-            }
+            handleEnded(gesture)
         default:
             ()
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let transform = gesture.translation(in: nil)
+        let degress: CGFloat = transform.x / 20
+        let angle = degress * .pi / 180
+        
+        let rotationalTransform = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransform.translatedBy(x: transform.x, y: transform.y)
+    }
+    
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        
+        let shouldDismissCard = gesture.translation(in: nil).x > 100  || gesture.translation(in: nil).x < -100
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut) {
+            
+            if shouldDismissCard {
+                if gesture.translation(in: nil).x > 100 {
+                    let offScreenTransform = self.transform.translatedBy(x: 1000, y: 0)
+                    self.transform = offScreenTransform
+                } else if gesture.translation(in: nil).x < -100 {
+                    let offScreenTransform = self.transform.translatedBy(x: -1000, y: 0)
+                    self.transform = offScreenTransform
+                }
+                
+            } else {
+                self.transform = .identity
+            }
+            
+        } completion: { _ in
+            
+            self.transform = .identity
         }
     }
 }
